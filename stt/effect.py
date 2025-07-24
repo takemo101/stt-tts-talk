70a244cb1bd73102cc5e0cae338018a332b1d8b8
takemo101 <takemo101@gmail.com>
2025-07-25 08:44:52 +0900

    音声認識機能に効果音再生機能を追加し、効果音プレイヤークラスを新規作成

---
+++ stt/effect.py
@@ -0,0 +1,26 @@
+import os
+from abc import ABC, abstractmethod
+
+
+class RecEffectPlayer(ABC):
+    """録音効果音の抽象クラス"""
+
+    @abstractmethod
+    def start(self) -> None:
+        """録音開始時の効果音を再生する"""
+        pass
+
+    @abstractmethod
+    def end(self) -> None:
+        """録音終了時の効果音を再生する"""
+        pass
+
+
+class RecEffectPlayerForMac(RecEffectPlayer):
+    """macOS用の録音効果音クラス"""
+
+    def start(self) -> None:
+        os.system('afplay /System/Library/Sounds/Tink.aiff')
+
+    def end(self) -> None:
+        os.system('afplay /System/Library/Sounds/Purr.aiff')
--- stt/speech_recognition.py
+++ stt/speech_recognition.py
@@ -2,6 +2,7 @@
 
 import speech_recognition as sr  # type: ignore
 
+from .effect import RecEffectPlayer, RecEffectPlayerForMac
 from .exceptions import SpeechRecognitionError
 
 
@@ -12,17 +13,18 @@ class SpeechRecognizer:
     AMBIENT_NOISE_DURATION = 1.0
 
     # 音声認識のタイムアウト時間
-    SPEECH_TIMEOUT = 5.0
+    SPEECH_TIMEOUT = 10.0
 
-    def __init__(self):
+    def __init__(
+        self, effect_player: RecEffectPlayer = RecEffectPlayerForMac()
+    ):
         self._recognizer = sr.Recognizer()
+        self._effect_player = effect_player
 
     def listen(self) -> str:
         """マイクから音声を取得し、テキストに変換する
-
         Returns:
             str: 認識されたテキスト
-
         Raises:
             SpeechRecognitionError: 音声認識に失敗した場合
         """
@@ -32,15 +34,20 @@ class SpeechRecognizer:
                 self._recognizer.adjust_for_ambient_noise(
                     source, duration=int(self.AMBIENT_NOISE_DURATION)
                 )
+                # 音声認識の開始時に効果音を再生
                 print('録音中... 話してください')
+                self._effect_player.start()
                 audio = self._recognizer.listen(
                     source, timeout=self.SPEECH_TIMEOUT
                 )
 
+            # 音声認識の終了時に効果音を再生
             print('音声認識中...')
             result: str = self._recognizer.recognize_google(  # type: ignore
                 audio, language='ja-JP'
             )
+
+            self._effect_player.end()
             print(f'認識結果: {result}')
             return result
 
